0e5abbd734f354ecb1ff1f0c3eadb5e888f6217f
Sergey Klimov <desire0417@gmail.com>
2019-04-19 13:54:10 +0300

    Метка на карте

--- source/js/ya-map.js
+++ source/js/ya-map.js
@@ -13,4 +13,13 @@
             // от 0 (весь мир) до 19.
             zoom: 16
         });
+        var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {}, {
+          iconLayout: 'default#image',
+          iconImageHref: '../img/icon-map-pin.svg',
+          iconImageSize: [66, 108],
+          iconImageOffset: [-35, -100]
+      });
+
+      myMap.geoObjects.add(myPlacemark);
+      myMap.controls.remove('zoomControl');
     }
